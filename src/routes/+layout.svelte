f6b4faec07aa8ab278fc44666a0b538dc1fd9327
Journey to Jah <onerootboii@proton.me>
2023-07-31 09:57:41 +0700

    Initial commit.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-crimson.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<img src="/poe2logo-resized.png" alt="" class="w-24 img-fluid" />
+				<strong class="text-xl uppercase">Poe2Wiki</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://discord.gg/CE46HADc5T"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Discord
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/Project-Path-of-Exile-Wiki"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,94 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {});
+</script>
+
+<div class="background" />
+<div class="overlay">
+	<div class="container text-wrap">
+		<h1 class="text-center h1">Path of Exile 2 Wiki</h1>
+		<p class="text-center">
+			Welcome to the Path of Exile 2 Wiki. This is a fan-made wiki for the upcoming game Path of
+			Exile 2. <br /> This wiki is currently a work in progress and will be updated as more information
+			is released.
+		</p>
+		<p class="text-center mt-12">
+			In the meantime, you can check out the{' '}
+			<a
+				href="https://www.poewiki.net/wiki/Path_of_Exile_2"
+				target="_blank"
+				class="btn btn-sm variant-ghost-primary"
+				rel="noopener noreferrer"
+				>Path of Exile 2 page
+			</a> on PoEWiki.net
+		</p>
+
+		<p class="text-center mt-4">
+			You can also visit the PoEWiki{' '}
+			<a
+				class="btn btn-sm variant-ghost-tertiary"
+				href="https://discord.gg/CE46HADc5T"
+				target="_blank"
+				rel="noreferrer"
+			>
+				Discord
+			</a>
+			server
+		</p>
+
+		<p class="text-center mt-4">
+			Fork Fandom! {' '}
+			<a
+				class="btn btn-sm variant-ghost-surface"
+				href="https://forkfandom.com/"
+				target="_blank"
+				rel="noreferrer">Read More</a
+			>
+		</p>
+
+		<!-- add a footnote saying we are not affiliated with GGG -->
+		<div class="bottom">
+			<p class="text-center">
+				This product isn't affiliated with or endorsed by Grinding Gear Games in any way.
+			</p>
+		</div>
+	</div>
+</div>
+
+<style lang="postcss">
+	.background {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-image: url('/backgrounds/background1.png');
+		background-repeat: no-repeat;
+		background-size: cover;
+		background-position: center;
+	}
+
+	.overlay {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(0, 0, 0, 0.6);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.text-wrap {
+		max-width: 800px;
+	}
+
+	.bottom {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		width: 100%;
+	}
+</style>
